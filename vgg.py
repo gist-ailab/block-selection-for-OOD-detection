55d2599ffa97a0da02de04ff333b59c299649ceb
birdomi <ryk012@naver.com>
2023-10-27 23:11:55 +0900

    vgg architecture code

---
+++ vgg.py
@@ -0,0 +1,64 @@
+'''VGG11/13/16/19 in Pytorch.'''
+import torch
+import torch.nn as nn
+import math
+
+cfg = {
+    'VGG11': [64, 'M', 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
+    'VGG13': [64, 64, 'M', 128, 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
+    'VGG16': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512, 'M', 512, 512, 512, 'M'],
+    'VGG19': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 256, 'M', 512, 512, 512, 512, 'M', 512, 512, 512, 512, 'M'],
+}
+
+
+class VGG(nn.Module):
+    def __init__(self, vgg_name, num_classes):
+        super(VGG, self).__init__()
+        self.features = self._make_layers(cfg[vgg_name])
+
+        self.classifier = nn.Sequential(                             
+            nn.Linear(512, 512),
+            nn.ReLU(True),
+            nn.Dropout(p=0.5),
+            nn.Linear(512 , 512),
+            nn.ReLU(True),
+            nn.Dropout(p=0.5),
+            nn.Linear(512, num_classes)
+        )
+        #  # Initialize weights
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2. / n))
+                m.bias.data.zero_() # for bias
+
+    def forward(self, x):
+        out = self.forward_features(x)
+        out = self.classifier(out)
+        return out
+
+    def forward_features(self,x):
+        out = self.features(x)
+        return out.view(out.size(0), -1)
+
+    def _make_layers(self, cfg):
+        layers = []
+        in_channels = 3
+        for x in cfg:
+            if x == 'M':
+                layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
+            else:
+                layers += [nn.Conv2d(in_channels, x, kernel_size=3, padding=1 ,bias=True), #bias=False
+                           nn.ReLU(inplace=True)]
+                in_channels = x
+        layers += [nn.AvgPool2d(kernel_size=1, stride=1)]
+        return nn.Sequential(*layers)
+
+
+def test():
+    net = VGG('VGG11')
+    x = torch.randn(2,3,32,32)
+    y = net(x)
+    print(y.size())
+
+# test()
